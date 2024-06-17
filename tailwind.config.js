56a6066b331b24a90186d3d6df7ad24ae9766851
Parithi95 <parithi95@gmail.com>
2024-06-17 16:12:20 +0530

    responsive added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      display: ["responsive"],
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
